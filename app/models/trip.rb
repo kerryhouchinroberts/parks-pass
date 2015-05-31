86a03671aca51b4dac1a99510399b6aa7768937d
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-05-31 16:11:16 -0600

    Write method to format dates

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,13 @@
 class Trip < ActiveRecord::Base
   belongs_to :park
   belongs_to :user
+
+  def start_formatted
+    start_date.strftime('%B %e, %Y')
+  end
+
+  def end_formatted
+    end_date.strftime('%B %e, %Y')
+  end
+
 end
