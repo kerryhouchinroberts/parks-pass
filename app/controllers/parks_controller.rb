d7bbb72fb4aefe2e89e9ce3732576e62f3c7152f
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-06-03 11:13:08 -0600

    User can see a carousel of recreation.gov photos on the park show page if photos are available

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,5 +1,4 @@
 class ParksController < ApplicationController
-  require 'wikipedia'
 
   def index
     @parks = Park.all
@@ -9,14 +8,17 @@ class ParksController < ApplicationController
   def show
     @park = Park.find(params[:id])
     insta_fetcher = InstaFetcher.new
-    @photos = insta_fetcher.park_pics(@park.latitude, @park.longitude, current_user.token)
-    @doi_photos = insta_fetcher.dept_of_interior_pics(current_user.token)
-    @wikipage = Wikipedia.find("#{@park.name} National Park")
-    @wiki = WikiCloth::Parser.new({
-      :data => @wikipage.raw_data["query"]["pages"][@wikipage.raw_data["query"]["pages"].keys[0]]["revisions"][0]["*"] })
+    if current_user != nil
+      @photos = insta_fetcher.park_pics(@park.latitude, @park.longitude, current_user.token)
+      @doi_photos = insta_fetcher.dept_of_interior_pics(current_user.token)
+    end
     rec_fetcher = RecFetcher.new
     @park_rec_info = rec_fetcher.park_info(@park.rec_id)
     @park_rec_photos = rec_fetcher.park_photos(@park.rec_id)
+    if @park_rec_photos["RECDATA"].count > 0
+      @first_park_rec_photo = @park_rec_photos["RECDATA"][0]
+      @remaining_park_rec_photos = @park_rec_photos["RECDATA"].drop(1)
+    end
     @park_links = rec_fetcher.park_links(@park.rec_id)
   end
 end
